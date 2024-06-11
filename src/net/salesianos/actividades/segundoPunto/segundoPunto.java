0470eb5bf4c471549bdd6aba32e8e93c9ed3b8d9
williamGanem <usr3852@salesianos-lacuesta.net>
2024-06-11 10:22:08 +0100

    feat: dentro de un bloque try-catch abrimos un flujo de entrada con el archivo pasado como parametro, luego declaramos c para almacenar el codigo ascii de cada caracter, con un bucle while iteramos cada caracter hasta que llegue a -1 que seria el final del archivo, y mientras el caracter no sea el ultimo mostrara por consola su caracter y sus codigo ascii separado por comas, por ultimos cerramos el flujo y controlamos exepciones

--- src/net/salesianos/actividades/segundoPunto/segundoPunto.java
+++ src/net/salesianos/actividades/segundoPunto/segundoPunto.java
@@ -1,18 +1,37 @@
 package net.salesianos.actividades.segundoPunto;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class segundoPunto {
     public static void main(String[] args) {
         String texto = "Hola, pibe.";
-        String ruta = "src\\net\\salesianos\\actividades\\segundoPunto\\segundoPunto.txt";
-        try (FileWriter writer = new FileWriter(ruta)) {
+        String rutaEscritura = "src\\net\\salesianos\\actividades\\segundoPunto\\segundoPunto.txt";
+        try (FileWriter writer = new FileWriter(rutaEscritura)) {
             writer.write(texto);
             System.out.println("Texto guardado en el fichero.");
             writer.close();
         } catch (IOException e) {
             System.out.println("No se ha podido crear el archivo");
         }
+
+        String rutaLectura = "src\\net\\salesianos\\actividades\\segundoPunto\\segundoPunto.txt";
+        try (FileReader entrada = new FileReader(rutaLectura)) {
+            int c = entrada.read();
+            while (c != -1) {
+                char letra = (char) c;
+                System.out.print(letra + "_" + c);
+                c = entrada.read();
+                if (c != -1) {
+                    System.out.print(",");
+                }
+            }
+
+            entrada.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
     }
 }
